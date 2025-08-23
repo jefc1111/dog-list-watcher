3a3bcaa571c88f8c9532c1ba6f6695d04d99a6ae
Geoff Clayton <jefc_uk@hotmail.com>
2025-08-23 22:13:43 +0100

    Make sure envvars are picked up in cron context

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 
 const chromium = require("@sparticuz/chromium")
 const puppeteer = require("puppeteer-core")
